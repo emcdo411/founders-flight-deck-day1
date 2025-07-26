d13db4c4d1890f95e9e97f6e84bf0a661612f1d5
Maurice McDonald <45975919+emcdo411@users.noreply.github.com>
2025-07-26 18:53:17 -0500

    Update create_ComplianceData.sql

--- sql/create_ComplianceData.sql
+++ sql/create_ComplianceData.sql
@@ -1,1 +1,14 @@
+USE FlightDeckDB;
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='ComplianceData' AND xtype='U')
+BEGIN
+    CREATE TABLE ComplianceData (
+        Date DATE,
+        Region VARCHAR(50),
+        ComplianceScore FLOAT,
+        RiskFlag BIT,
+        PRIMARY KEY (Date, Region)
+    );
+END;
 
