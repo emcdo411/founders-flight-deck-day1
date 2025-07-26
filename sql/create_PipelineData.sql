c1d1492f909aac86abb41082036860484dfa7bbc
Maurice McDonald <45975919+emcdo411@users.noreply.github.com>
2025-07-26 18:51:57 -0500

    Update create_PipelineData.sql

--- sql/create_PipelineData.sql
+++ sql/create_PipelineData.sql
@@ -1,1 +1,15 @@
+USE FlightDeckDB;
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='PipelineData' AND xtype='U')
+BEGIN
+    CREATE TABLE PipelineData (
+        Date DATE,
+        Sector VARCHAR(50),
+        FlagRate FLOAT,
+        FairnessScore FLOAT,
+        KPI_Index FLOAT,
+        PRIMARY KEY (Date, Sector)
+    );
+END;
 
